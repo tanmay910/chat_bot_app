d2b04d01b86e2316637cfbbf550cb852a851269e
I-Zz <21bds022@iiitdwd.ac.in>
2023-05-01 05:03:17 +0530

    added an empty top tabs screen

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,12 @@ import 'package:provider/provider.dart';
 import 'constants/constants.dart';
 import 'providers/chats_provider.dart';
 
-import 'screens/chat_screen.dart';
-import 'screens/sign_in_screen.dart';
-import 'screens/sign_up_screen.dart';
+import './screens/chat_screen.dart';
+import './screens/sign_in_screen.dart';
+import './screens/sign_up_screen.dart';
 import 'providers/images_provider.dart';
 import './screens/text_image_tabs_screen.dart';
-
+import './screens/tabs_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -42,9 +42,7 @@ class MyApp extends StatelessWidget {
             appBarTheme: AppBarTheme(
               color: cardColor,
             )),
-
         home: TextImageTabsScreen(),
-
       ),
     );
   }
