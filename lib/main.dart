229af22d915e308d8b8cf06e9f8fedb6f596cca6
Milind Murmu <milindmurmu@gmail.com>
2023-05-01 03:17:15 +0530

    firebase authentication done

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,20 @@
 import 'package:chatgpt_course/providers/models_provider.dart';
+import 'package:chatgpt_course/screens/home_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'constants/constants.dart';
 import 'providers/chats_provider.dart';
-
 import 'screens/chat_screen.dart';
 import 'screens/sign_in_screen.dart';
 import 'screens/sign_up_screen.dart';
 import 'providers/images_provider.dart';
 import './screens/text_image_tabs_screen.dart';
 
-
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -42,9 +44,7 @@ class MyApp extends StatelessWidget {
             appBarTheme: AppBarTheme(
               color: cardColor,
             )),
-
-        home: TextImageTabsScreen(),
-
+        home: SignInPage(),
       ),
     );
   }
--- lib/screens/sign_in_screen.dart
+++ lib/screens/sign_in_screen.dart
@@ -1,11 +1,22 @@
+import 'package:chatgpt_course/screens/home_screen.dart';
+import 'package:chatgpt_course/screens/sign_up_screen.dart';
 import 'package:chatgpt_course/utils/global.colors.dart';
 import 'package:chatgpt_course/widgets/button.global.dart';
 import 'package:chatgpt_course/widgets/social.login.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:hexcolor/hexcolor.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import '../auth.dart';
+import 'sign_up_screen.dart';
+
+final _formkey = GlobalKey<FormState>();
+final _auth = FirebaseAuth.instance;
 
 class SignInPage extends StatefulWidget {
+  String? errorMessage = '';
+  bool isLogin = true;
+
   @override
   _SignInPageState createState() => _SignInPageState();
 }
@@ -14,11 +25,75 @@ class _SignInPageState extends State<SignInPage> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
-  void _signIn() {
-    // Add your sign in logic here
-    String email = _emailController.text;
-    String password = _passwordController.text;
-    print('Signing in with email: $email and password: $password');
+  bool showspinner = false;
+  String errorMessage = '';
+
+  void Signin({
+    required String email,
+    required String password,
+  }) async {
+    if (_formkey.currentState!.validate()) {
+      try {
+        setState(() {
+          showspinner = true;
+        });
+        _formkey.currentState!.save();
+
+        await _auth
+            .signInWithEmailAndPassword(email: email, password: password)
+            .then((Uid) => {
+                  showspinner = false,
+                  _formkey.currentState!.reset(),
+                  _emailController.clear(),
+                  _passwordController.clear(),
+                  Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(builder: (ctx) => HomePage())),
+                });
+      } on FirebaseAuthException catch (error) {
+        _formkey.currentState!.reset();
+        _emailController.clear();
+        _passwordController.clear();
+        switch (error.code) {
+          case "invalid-email":
+            errorMessage = "Your email address appears to be malformed.";
+            break;
+          case "wrong-password":
+            errorMessage = "Your password is wrong.";
+            break;
+          case "user-not-found":
+            errorMessage = "User with this email doesn't exist.";
+            break;
+          case "user-disabled":
+            errorMessage = "User with this email has been disabled.";
+            break;
+          case "too-many-requests":
+            errorMessage = "Too many requests";
+            break;
+          case "operation-not-allowed":
+            errorMessage = "Signing in with Email and Password is not enabled.";
+            break;
+          default:
+            errorMessage = "An undefined Error happened.";
+        }
+        setState(() {
+          showspinner = false;
+        });
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text(errorMessage!),
+            backgroundColor: Colors.red.withOpacity(0.9),
+            elevation: 2,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            behavior: SnackBarBehavior.floating,
+            duration: Duration(seconds: 2),
+          ),
+        );
+
+        // print(error.code);
+      }
+    }
   }
 
   @override
@@ -29,77 +104,119 @@ class _SignInPageState extends State<SignInPage> {
           child: Container(
             width: double.infinity,
             padding: const EdgeInsets.all(15.0),
-            child: Column(
-              children: [
-                const SizedBox(
-                  height: 100,
-                ),
-                Text(
-                  'Logo',
-                  style: TextStyle(
-                    color: GlobalColors.mainColor,
-                    fontSize: 35,
-                    fontWeight: FontWeight.bold,
+            child: Form(
+              key: _formkey,
+              child: Column(
+                children: [
+                  const SizedBox(
+                    height: 100,
                   ),
-                ),
-                const SizedBox(
-                  height: 100,
-                ),
-                Column(
-                  children: [
-                    Text(
-                      'Sign in to your account',
-                      style: TextStyle(
-                        color: GlobalColors.textColor,
-                        fontSize: 22,
-                        fontWeight: FontWeight.w500,
-                      ),
+                  Text(
+                    'Logo',
+                    style: TextStyle(
+                      color: GlobalColors.mainColor,
+                      fontSize: 35,
+                      fontWeight: FontWeight.bold,
                     ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(6.0),
-                      child: TextField(
-                        controller: _emailController,
-                        decoration: const InputDecoration(
-                          labelText: 'Email',
-                          hintText: 'Enter your email',
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 16),
-                    Padding(
-                      padding: const EdgeInsets.all(6.0),
-                      child: TextField(
-                        controller: _passwordController,
-                        obscureText: true,
-                        decoration: const InputDecoration(
-                          labelText: 'Password',
-                          hintText: 'Enter your password',
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 40),
-                    const ButtonGlobal(),
-                    const SizedBox(height: 30),
-                    Container(
-                      alignment: Alignment.center,
-                      child: const Text(
-                        '- Or Sign In with -',
+                  ),
+                  const SizedBox(
+                    height: 100,
+                  ),
+                  Column(
+                    children: [
+                      Text(
+                        'Sign in to your account',
                         style: TextStyle(
-                          color: Colors.white,
+                          color: GlobalColors.textColor,
+                          fontSize: 22,
+                          fontWeight: FontWeight.w500,
                         ),
                       ),
-                    ),
-                    const SocialLogin(),
-                  ],
-                ),
-              ],
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(6.0),
+                        child: TextFormField(
+                          controller: _emailController,
+                          validator: (value) {
+                            if (value!.isEmpty) return 'Please Enter Email';
+                            return null;
+                          },
+                          decoration: const InputDecoration(
+                            labelText: 'Email',
+                            hintText: 'Enter your email',
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 16),
+                      Padding(
+                        padding: const EdgeInsets.all(6.0),
+                        child: TextFormField(
+                          controller: _passwordController,
+                          obscureText: true,
+                          validator: (value) {
+                            if (value!.isEmpty) return 'Please Enter password';
+                            return null;
+                          },
+                          decoration: const InputDecoration(
+                            labelText: 'Password',
+                            hintText: 'Enter your password',
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 40),
+                      InkWell(
+                        onTap: () {
+                          Signin(
+                              email: _emailController.text,
+                              password: _passwordController.text);
+                        },
+                        child: Container(
+                          alignment: Alignment.center,
+                          height: 55,
+                          decoration: BoxDecoration(
+                            color: GlobalColors.mainColor,
+                            borderRadius: BorderRadius.circular(6),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withOpacity(0.1),
+                                blurRadius: 10,
+                              ),
+                            ],
+                          ),
+                          child: showspinner
+                              ? const CircularProgressIndicator(
+                                  color: Colors.white,
+                                )
+                              : const Text(
+                                  'Sign in',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                        ),
+                      ),
+                      const SizedBox(height: 30),
+                      Container(
+                        alignment: Alignment.center,
+                        child: const Text(
+                          '- Or Sign In with -',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                      const SocialLogin(),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
@@ -110,19 +227,22 @@ class _SignInPageState extends State<SignInPage> {
         child: Row(
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
+          children: [
             Text(
               'Don\'t have an account',
             ),
-            SizedBox(
+            const SizedBox(
               width: 5,
             ),
             InkWell(
-              child: Text(
-                'Sign Up',
-                style: TextStyle(
-                  color: Colors.white,
-                ),
+              child: TextButton(
+                child: const Text('Sign Up'),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => SignUpPage()),
+                  );
+                },
               ),
             ),
           ],
@@ -131,3 +251,14 @@ class _SignInPageState extends State<SignInPage> {
     );
   }
 }
+
+
+// TextButton(
+//   onPressed: () {
+//     Navigator.push(
+//       context,
+//       MaterialPageRoute(builder: (context) => NewPage()),
+//     );
+//   },
+//   child: Text("Go to New Page"),
+// ),
